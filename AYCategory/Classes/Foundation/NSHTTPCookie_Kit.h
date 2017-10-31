634179e195be2f5af1ada6a5009e8c8142db3b03
Alan Yeh <alan@yerl.cn>
2017-10-31 15:42:51 +0800

    添加HTTPCookie的便利方法

---
+++ AYCategory/Classes/Foundation/NSHTTPCookie_Kit.h
@@ -0,0 +1,12 @@
+//
+//  NSHTTPCookie_Kit.h
+//  AYCategory
+//
+//  Created by Alan Yeh on 2017/10/31.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSHTTPCookie (Kit)
+- (NSString *)ay_cookieString;
+@end
---
+++ AYCategory/Classes/Foundation/NSHTTPCookie_Kit.m
@@ -0,0 +1,23 @@
+//
+//  NSHTTPCookie_Kit.m
+//  AYCategory
+//
+//  Created by Alan Yeh on 2017/10/31.
+//
+
+#import "NSHTTPCookie_Kit.h"
+#import "NSString_Kit.h"
+
+@implementation NSHTTPCookie (Kit)
+- (NSString *)ay_cookieString{
+    NSString *result = NSStringWithFormat(@"%@=%@;domain=%@;path=%@", self.name, self.value, self.domain, self.path);
+    if (self.isSecure) {
+        result = [result stringByAppendingString:@";secure"];
+    }
+    if (self.isHTTPOnly) {
+        result = [result stringByAppendingString:@";HttpOnly"]
+    }
+    return result;
+}
+@end
+
