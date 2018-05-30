ae186ac537aea059dc306da5764a6f3b6a944df4
Alan Yeh <alan@yerl.cn>
2018-05-30 14:11:12 +0800

    更新MD5，转成大写

--- AYCategory/Classes/Foundation/NSString_Encyption.m
+++ AYCategory/Classes/Foundation/NSString_Encyption.m
@@ -26,7 +26,7 @@
             result[10], result[11],
             result[12], result[13],
             result[14], result[15]
-            ];
+            ].uppercaseString;
 }
 
 #pragma mark - 3DES
